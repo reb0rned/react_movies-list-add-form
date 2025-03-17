19141b838612a761be61823b4dd09d9f0feb91ef
reb0rned <affmarket77@gmail.com>
2025-03-17 14:12:19 +0200

    films adding resolving

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,120 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie'
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type Props = {
+  onAdd: (movie: Movie) => void
+}
+
+export const NewMovie: React.FC<Props> = ({onAdd}) => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [imgUrl, setImgUrl] = useState('')
+  const [imdbUrl, setImdbUrl] = useState('')
+  const [imdbId, setImdbId] = useState('')
+
+  const formFieldHandler = (field: string, value: string) => {
+    if (field) {
+      switch (field) {
+        case 'title':
+          setTitle(value)
+        break;
+
+        case 'description':
+          setDescription(value)
+        break;
+
+        case 'imgUrl':
+          setImgUrl(value)
+        break;
+
+        case 'imdbUrl':
+          setImdbUrl(value)
+        break;
+
+        case 'imdbId':
+          setImdbId(value)
+        break;
+      }
+    }
+  }
+
+  const isFilled = title !== ''
+  && imgUrl !== ''
+  && imdbUrl !== ''
+  && imdbId !== ''
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault()
+
+    if (isFilled) {
+      const newMovie = {
+        title,
+        description: description || '',
+        imgUrl,
+        imdbUrl,
+        imdbId
+      };
+
+      onAdd(newMovie)
+      setCount(count + 1)
+
+      setTitle('');
+      setDescription('');
+      setImgUrl('');
+      setImdbUrl('');
+      setImdbId('');
+    }
+  }
+
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+    className="NewMovie"
+    key={count}
+    onSubmit={submitHandler}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={(value) => formFieldHandler('title', value)}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+      name="description"
+      label="Description"
+      value={description}
+      onChange={(value) => formFieldHandler('description', value)}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+      name="imgUrl"
+      label="Image URL"
+      value={imgUrl}
+      onChange={(value) => formFieldHandler('imgUrl', value)}
+      required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+      name="imdbUrl"
+      label="Imdb URL"
+      value={imdbUrl}
+      onChange={(value) => formFieldHandler('imdbUrl', value)}
+      required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+      name="imdbId"
+      label="Imdb ID"
+      value={imdbId}
+      onChange={(value) => formFieldHandler('imdbId', value)}
+      required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +122,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isFilled ? true : false}
           >
             Add
           </button>
